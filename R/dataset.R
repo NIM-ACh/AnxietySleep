d11841e8ee9f33047dffe906cf3607fe76c5d71c
Matías Castillo <matcasti@umag.cl>
2022-12-09 12:48:52 -0300

    adding examples to data documentation

--- R/dataset.R
+++ R/dataset.R
@@ -1,7 +1,11 @@
 #' Data from the anxiety and confinement study.
 #'
+#' @description
+#' `r lifecycle::badge('stable')`
+#'
 #' This dataset contains the variables used in the anxiety and confinement study carried out
 #' by Alvarado-Aravena et al. 2022.
+#'
 #'
 #' @format A data frame with 617 rows y 7 variables:
 #'
@@ -15,6 +19,25 @@
 #'   \item{\code{cat_age}: Factor w/ 4 levels "18-25", "26-40", "41-50", ">50". Age of the subjects in years.}
 #' }
 #'
+#' @examples
+#' # Mean age grouped by sex and zone using `data.table` syntax
+#' anxiety[,  # No filtering (i)
+#'         list(mean_age = mean(age)), # Action to do (j)
+#'         list(sex, zone)] # Grouping vars (by)
+#'
+#' # Mean PSQI score grouped by sex and zone, for those with
+#' # an age greater than 18 AND a Beck score greater than 10.
+#' anxiety[age > 18 & beck_global > 10,
+#'         list(mean_psqi = mean(pits_global)),
+#'         list(sex, zone)]
+#'
 #' @source
-#'  -  Alvarado-Aravena, C., Arriaza, K., Castillo-Aguilar, M., Flores, K., Dagnino-Subiabre, A., Estrada-Goic, C., & Núñez-Espinosa, C. (2022). Effect of Confinement on Anxiety Symptoms and Sleep Quality during the COVID-19 Pandemic. Behavioral Sciences, 12(10), 398.
+#'  -  Alvarado-Aravena, C., Arriaza, K., Castillo-Aguilar, M., Flores, K.,
+#'  Dagnino-Subiabre, A., Estrada-Goic, C., & Núñez-Espinosa, C. (2022). Effect
+#'  of Confinement on Anxiety Symptoms and Sleep Quality during the COVID-19
+#'  Pandemic. Behavioral Sciences, 12(10), 398.
 "anxiety"
+
+
+
+
